ec6e54b77a4c6cd152598e51754de2a2bfb29939
Tanuja <kollt01@pfw.edu>
2024-03-24 11:08:37 -0400

    Added code fixes

--- App.js
+++ App.js
@@ -10,14 +10,7 @@ import RunDetails from './startNewRun/RunDetails';
 import LogActivityScreen from './logPreviousActivity/LogActivityScreen';
 import ProfileScreen from './profile/ProfileScreen';
 import HistoryScreen from './history/HistoryScreen';
-import SignupForm from './SignupForm';
-import LoginForm from './LoginForm';
-import HomeScreen from './HomeScreen';
-import FirstTime from './FirstTime';
-import StartScreen from './StartScreen';
-import RunDetails from './RunDetails';
-import LogActivityScreen from './LogActivityScreen';
-import ForgotPassword from './ForgotPassword'; // Ensure this file exists and is correctly exported
+import ForgotPassword from './newUserRegistration/ForgotPassword'; // Ensure this file exists and is correctly exported
 
 const Stack = createStackNavigator();
 
@@ -25,7 +18,6 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoginForm">
-      <Stack.Navigator initialRouteName="HomeScreen" unmountInactiveRoutes={true}>
         <Stack.Screen name="FirstTime" component={FirstTime} options={{ headerShown: false }} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="LoginForm" component={LoginForm} />
@@ -35,7 +27,6 @@ function App() {
         <Stack.Screen name="LogActivityScreen" component={LogActivityScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="HistoryScreen" component={HistoryScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="LogActivityScreen" component={LogActivityScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       </Stack.Navigator>
     </NavigationContainer>
